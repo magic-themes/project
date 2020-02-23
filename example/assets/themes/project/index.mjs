84752ac82821e2dbd45f0ac6d3a5b9b1cd918b46
jascha <jascha@jaeh.at>
2020-02-23 18:56:26 +0100

    local theme: export state

--- example/assets/themes/project/index.mjs
+++ example/assets/themes/project/index.mjs
@@ -1,2 +1,2 @@
 // this "hack" allows us to include the locally developed theme
-export { default, Footer, Header, Hero, Logo, vars } from '../../../../src/index.mjs'
+export { default, Footer, Header, Hero, Logo, state, vars } from '../../../../src/index.mjs'
