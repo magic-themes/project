32a34607a2227428ada198a1c16f274a234646b0
jascha <jascha@jaeh.at>
2020-03-05 17:09:55 +0100

    src/index: use content field in default data

--- src/index.mjs
+++ src/index.mjs
@@ -7,6 +7,7 @@ export const state = {
   hero: {
     title: 'hero module',
     description: 'short branding slogan',
+    content: "some additional text. optional.",
     menu: [
       { to: '/#about', text: 'about' },
       { to: '/#usage', text: 'usage' },
